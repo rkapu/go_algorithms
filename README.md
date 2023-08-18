a51dc8e3a8291d40485b7813e5b706ff1f897035
rkapu <romankap1798@gmail.com>
2023-08-18 20:58:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# go_algorithms
+Implementation of different algorithms for `The Last Algorithms Course You'll Need` course from Primagen on FrontendMasters
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Implementation of different algorithms for `The Last Algorithms Course You'll Need` course from Primagen on FrontendMasters")
+}
